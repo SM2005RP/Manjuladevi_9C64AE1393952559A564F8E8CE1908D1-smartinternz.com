04902fc95776c1ebdc4d2dfd5a7cb45d3a805f38
SM2005RP <145100259+SM2005RP@users.noreply.github.com>
2023-09-15 12:21:32 +0530

    Add files via upload

---
+++ challenge unit 1/main.py
@@ -0,0 +1,10 @@
+def fact_recc(n):
+  if n == 0 or n == 1:
+    return 1
+  else:
+    return n * fact_recc(n - 1)
+
+
+number = int(input('enter a value:'))
+res = fact_recc(number)
+print("the  factorial  of{} is {}".format(number, res))
